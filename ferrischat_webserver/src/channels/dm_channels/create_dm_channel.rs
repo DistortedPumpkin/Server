cf83a3658cd76e3754402470c4ac9758e89813bd
DistortedPumpkin <therealdistortedpumpkin@gmail.com>
2021-12-30 14:58:48 -0500

    change query

--- ferrischat_webserver/src/channels/dm_channels/create_dm_channel.rs
+++ ferrischat_webserver/src/channels/dm_channels/create_dm_channel.rs
@@ -23,24 +23,28 @@ pub async fn create_dm_channel(
     
     if !group && users.len() > 1 {
         return Err(ErrorJson::new_400("Direct DM messages can not contain more than 1 other user. \
-         Consider making this a group DM to include more people".to_string()).into());
+        Consider making this a group DM to include more people".to_string()).into());
     }
 
     users.push(bigint_authed_user);
 
-    sqlx::query!(
-        "INSERT INTO dmchannels VALUES ($1, $2, $3, $4)",
+    let res_users = sqlx::query!(
+        r#"
+        INSERT INTO dmchannels VALUES ($1, $2, $3);
+
+        FOR uid IN $4 LOOP
+            INSERT INTO dmmembers VALUES (uid, $1);
+        END LOOP;
+
+        SELECT * FROM users WHERE id = ANY($4)
+        "#,
         bigint_channel_id,
-        users,
         group,
         name,
+        users
     )
-    .execute(db)
+    .fetch_all(db)
     .await?;
-
-    let res_users = sqlx::query!("SELECT * FROM users WHERE id = ANY($1)", users)
-        .fetch_all(db)
-        .await?;
 
     let mut users = Vec::with_capacity(res_users.len());
     for u in res_users {
@@ -54,14 +58,14 @@ pub async fn create_dm_channel(
             pronouns: u
                 .pronouns
                 .and_then(ferrischat_common::types::Pronouns::from_i16),
-        })
+        });
     }
     
     let dm_channel_obj = DMChannel {
         id: bigint_channel_id,
         name,
         users,
-        group
+        group,
     };
 
     Ok(crate::Json {
